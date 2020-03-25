c207ab778877fc8ac914fac9866667b703b4a7fa
Cindy <ziishe1995@gmail.com>
2020-03-25 16:19:53 +0800

    mutipleImageUpload

--- src/components/mutipleUpload.vue
+++ src/components/mutipleUpload.vue
@@ -1,64 +1,34 @@
 <template>
-  <div :class="cls.container">
-    <div :class="cls.area" @drop.prevent="onDrop" @dragover.prevent="dragOver = true" @dragleave.prevent="dragOver = false" @paste.prevent="onPaste">
-      <div v-if="!imageList.length" :class="cls.tips">
-        <div>请将档案拖曳至此,</div>
-        <div>或是点击“+”选择档案</div>
-        <!-- <div :class="cls.subtitle">{{ subtitle }}</div> -->
-      </div>
+  <div class="dropzone-box">
+    <div id="dropzone" @drop.prevent="onDrop" @dragover.prevent="dragOver = true" @dragleave.prevent="dragOver = false">
+      <form class="dropzone dz-clickable" id="my-awesome-dropzone">
+        <label for="avatar" style="width:100%">
+          <div class="dz-default dz-message"><span>將檔案拖放到<b>此處</b>或<b>點擊</b>上傳。</span></div>
+        </label>
+        <div
+          v-for="(item, index) of imageList"
+          :key="index"
+          class="dz-preview dz-processing dz-error dz-complete dz-image-preview"
+          style="position: relative;"
+        >
+          <div class="dz-image">
+            <img :src="item.src" data-dz-thumbnail alt="" />
+          </div>
+          <div class="dz-details">
+            <div class="dz-filename">
+              <span data-dz-name="">{{ item.name }}</span>
+            </div>
+          </div>
+          <a class="dz-remove" @click="doDelete(item.id)">Delete</a>
+        </div>
+      </form>
 
-      <div v-for="(item, index) of imageList" :title="item.name" :key="index" :class="cls.item" :data-title="item.name">
-        <img :src="item.src" />
-        <span :class="cls.close" @click="doDelete(item.id)">&times;</span>
-      </div>
-
-      <label :for="nextId" :class="cls.img">
-        <span :class="cls.choice">+</span>
-      </label>
-      <input :id="nextId" @change.prevent="onChoice" multiple style="display: none !important;" type="file" />
+      <input id="avatar" @change.prevent="onChoice" multiple style="display: none !important;" type="file" />
     </div>
   </div>
 </template>
 <script>
 import ImageMixins from 'mixins/Image'
-var namespace = 'im'
-
-function toClass(name) {
-  if (namespace) {
-    if (namespace[-1] === '-') {
-      return namespace + name
-    } else {
-      return namespace + '-' + name
-    }
-  } else {
-    return name
-  }
-}
-
-toClass.useSubNS = function(ns) {
-  var prefix = toClass(ns).trim()
-
-  prefix = prefix ? (prefix[-1] === '-' ? prefix : prefix + '-') : prefix
-  return function(name) {
-    return name ? prefix + name : prefix.slice(0, -1)
-  }
-}
-
-const creator = toClass.useSubNS('upload')
-
-const classes = {
-  container: creator('container'),
-  area: creator('area'),
-  item: creator('item'),
-  img: creator('add'),
-  choice: creator('sign'),
-  foot: creator('foot'),
-  close: creator('close'),
-  tips: creator('tips'),
-  subtitle: creator('subtitle'),
-}
-
-const megaByte = 1024 * 1024
 
 export default {
   mixins: [ImageMixins],
@@ -68,7 +38,6 @@ export default {
       default: [],
     },
     imageList: { type: Array, required: true },
-    subtitle: { type: String, default: '' },
   },
   computed: {
     nextId: function() {
@@ -111,149 +80,3 @@ export default {
   },
 }
 </script>
-
-<style>
-.im-upload-container {
-  border: 1px solid #d2d2d2;
-  border-radius: 0.3em;
-  margin-bottom: 15px;
-}
-
-.im-upload-container .im-upload-foot {
-  display: block;
-  height: 46px;
-  line-height: 46px;
-  padding: 0 15px;
-  border-top: 1px solid #d2d2d2;
-}
-
-.im-upload-tips {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  user-select: none;
-}
-
-.im-upload-area {
-  position: relative;
-  display: block;
-  padding: 15px 0 0 15px;
-}
-
-.im-upload-area:hover {
-  box-shadow: 0 0 0 1px #d2d2d2 inset;
-}
-
-.im-upload-area > .im-upload-tips {
-  color: #ddd;
-  font-size: 22px;
-  cursor: default;
-  text-align: center;
-}
-
-.im-upload-area > .im-upload-tips > .im-upload-subtitle {
-  font-size: 18px;
-}
-
-.im-upload-area > .im-upload-item {
-  position: relative;
-  display: inline-block;
-  width: 100px;
-  height: 100px;
-  margin-bottom: 15px;
-  margin-right: 15px;
-  overflow: hidden;
-  box-shadow: 0 0 0 0 #d2d2d2;
-  transition-duration: 0.2s;
-  border-radius: 0.3em;
-}
-
-.im-upload-area > .im-upload-item.im-uploaded {
-  box-shadow: 0 0 0 3px #008b45;
-}
-
-.im-upload-area > .im-upload-item.im-uploaded:hover {
-  box-shadow: 0 0 5px 5px #006231;
-}
-
-.im-upload-area > .im-upload-item::before {
-  content: attr(data-title);
-  display: block;
-  width: 100%;
-  height: 0;
-  position: absolute;
-  background: rgba(35, 35, 35, 0.6);
-  line-height: 25px;
-  text-align: center;
-  color: #fefefe;
-  overflow: hidden;
-  transition-duration: 0.2s;
-}
-
-.im-upload-area > .im-upload-item:hover {
-  box-shadow: 0 0 5px 5px #bebebe;
-}
-
-.im-upload-area > .im-upload-item:hover::before {
-  height: 25px;
-}
-
-.im-upload-area > .im-upload-item > .im-upload-close {
-  cursor: pointer;
-  position: absolute;
-  display: block;
-  top: 3px;
-  right: 3px;
-  z-index: 1;
-  background: #000;
-  color: #ffffff;
-  width: 18px;
-  height: 18px;
-  text-align: center;
-  border-radius: 50%;
-}
-
-.im-upload-area > .im-upload-item > img {
-  vertical-align: middle;
-  height: 100%;
-}
-
-.im-upload-add {
-  margin-bottom: 15px;
-  position: relative;
-  display: inline-block;
-  width: 100px;
-  height: 100px;
-  border: 3px dashed #d2d2d2;
-  transition-duration: 0.2s;
-  vertical-align: top;
-  -webkit-box-sizing: border-box;
-  -moz-box-sizing: border-box;
-  box-sizing: border-box;
-  border-radius: 0.3em;
-  cursor: pointer;
-}
-
-.im-upload-add > .im-upload-sign {
-  position: absolute;
-  top: 40%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  font-size: 100px;
-  color: #d2d2d2;
-  transition-duration: 0.2s;
-}
-
-.im-upload-add > .im-upload-sign:hover {
-  color: #bebebe;
-}
-
-.im-upload-add:hover {
-  border-color: #bebebe;
-}
-
-.float-right {
-  float: right;
-}
-</style>
