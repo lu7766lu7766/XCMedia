b7ff0b2be0e4f760c5bcedffcb90757d3681523d
Cindy <ziishe1995@gmail.com>
2020-03-27 16:18:12 +0800

    帳號管理:角色多選

--- src/pages/SysSetting/AccountManage/modal/update.vue
+++ src/pages/SysSetting/AccountManage/modal/update.vue
@@ -1,19 +1,16 @@
 <template>
   <detail title="编辑" @submit="doSubmit()">
-
     <div class="form-group row m-b-15">
       <label class="col-md-2 col-form-label required">帐号</label>
       <div class="col-md-10">
-        <input type="text" class="form-control"
-               :value="data.account" disabled />
+        <input type="text" class="form-control" :value="data.account" disabled />
       </div>
     </div>
     <div class="form-group row m-b-15">
       <label class="col-md-2 col-form-label required">昵称</label>
       <div class="col-md-10">
         <validate rules="required|min:4|max:32">
-          <input type="text" class="form-control"
-                 v-model="data.display_name" />
+          <input type="text" class="form-control" v-model="data.display_name" />
         </validate>
       </div>
     </div>
@@ -21,8 +18,7 @@
       <label class="col-md-2 col-form-label">密码</label>
       <div class="col-md-10">
         <validate rules="min:4|max:32" vid="password">
-          <input type="password" class="form-control"
-                 v-model="data.password" />
+          <input type="password" class="form-control" v-model="data.password" />
         </validate>
       </div>
     </div>
@@ -30,20 +26,24 @@
       <label class="col-md-2 col-form-label">密码确认</label>
       <div class="col-md-10">
         <validate rules="confirmed:password">
-          <input type="password" class="form-control"
-                 v-model="data.password_confirmation" />
+          <input type="password" class="form-control" v-model="data.password_confirmation" />
         </validate>
         <!--<div class="m-t-1 form-txt text-red">需英数组合, 4~16字元</div>-->
       </div>
     </div>
+
     <div class="form-group row m-b-15">
-      <label class="col-md-2 control-label required">角色</label>
+      <label class="col-md-2 col-form-label required">角色</label>
       <div class="col-md-10">
         <validate rules="required">
-          <multi-select :options="_.map(options.roles, role => ({ value: role.id, text: role.display_name }))"
-                        :selected-options="data.roles"
-                        @select="item => data.roles = item"
-                        v-model="data.roles" />
+          <multi-list-select
+            :list="options.roles"
+            optionValue="id"
+            optionText="display_name"
+            :selectedItems="data.role_id"
+            @select="item => (data.role_id = item)"
+            v-model="data.role_id"
+          />
         </validate>
       </div>
     </div>
@@ -64,37 +64,32 @@
 </template>
 
 <script>
-  import DetailMixins from 'mixins/Detail'
-  import { MultiSelect } from 'vue-search-select'
+import DetailMixins from 'mixins/Detail'
+import { ModelListSelect, MultiListSelect } from 'vue-search-select'
 
-  export default {
-    mixins: [DetailMixins],
-    components: {
-      MultiSelect,
+export default {
+  mixins: [DetailMixins],
+  components: {
+    ModelListSelect,
+    MultiListSelect,
+  },
+  methods: {
+    async doSubmit() {
+      this.data.role_id = _.map(this.data.role_id, 'id')
+      let res = await this.$thisApi.doUpdate(this.data)
+      this.updateSuccess()
     },
-    methods: {
-      async doSubmit()
-      {
-        this.data.role_id = _.map(this.data.roles, 'value')
-        await this.$thisApi.doUpdate(this.data)
-        this.updateSuccess()
-      },
-    },
-    mounted()
-    {
-      this.$bus.on('update.show', data =>
-      {
-        this.data = _.cloneDeep(data)
-        this.data.roles = _.map(this.data.roles, role => ({
-          value: role.id,
-          text: role.display_name,
-        }))
-        this.show()
-      })
-    },
-    destroyed()
-    {
-      this.$bus.off('update.show')
-    },
-  }
+  },
+  mounted() {
+    this.$bus.on('update.show', data => {
+      this.data = Object.assign({ role_id: [] }, data)
+      this.data.role_id = data.roles
+
+      this.show()
+    })
+  },
+  destroyed() {
+    this.$bus.off('update.show')
+  },
+}
 </script>
