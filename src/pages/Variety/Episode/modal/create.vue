5d0beea8e1bddc33e54b30b3197532b95f15791c
Summer <mp4bj6ruby@gmail.com>
2020-04-06 14:32:43 +0800

    戲劇管理、電影管理、動漫管理、綜藝管理 集數設定的來源bug修正

--- src/pages/Variety/Episode/modal/create.vue
+++ src/pages/Variety/Episode/modal/create.vue
@@ -1,20 +1,23 @@
 <template>
   <detail title="新增" @submit="doSubmit()">
-
     <div class="form-group row m-b-15">
       <label class="col-md-2 col-form-label required">名称 </label>
       <div class="col-md-10">
         <validate rules="required">
-          <input type="text" class="form-control" placeholder="请输入名称"
-                 v-model="data.title" />
+          <input
+            v-model="data.title"
+            type="text"
+            class="form-control"
+            placeholder="请输入名称"
+          >
         </validate>
       </div>
     </div>
 
-    <div class="form-group row m-b-15" v-if="data.sources_url" v-for="(source, index) in options.source" :key="index">
+    <div v-for="(source, index) in options.source" :key="index" class="form-group row m-b-15">
       <label class="col-md-2 col-form-label">{{ source.title }} </label>
       <div class="col-md-10">
-        <input type="text" class="form-control" placeholder="请输入网址" v-model="data.sources_url[source.id]" />
+        <input v-if="data.sources_url" v-model="data.sources_url[source.id]" type="text" class="form-control" placeholder="请输入网址">
       </div>
     </div>
 
@@ -25,10 +28,9 @@
           <div class="input-group">
             <date-time-picker v-model="data.opening_time" />
             <div class="input-group-append">
-              <span class="input-group-text"><i class="far fa-calendar-alt"></i></span>
+              <span class="input-group-text"><i class="far fa-calendar-alt" /></span>
             </div>
           </div>
-
         </validate>
       </div>
     </div>
@@ -39,39 +41,38 @@
         <switcher v-model="data.status" />
       </div>
     </div>
-
   </detail>
 </template>
 
 <script>
-  import DetailMixins from 'mixins/Detail'
-  import ImageMixins from 'mixins/Image'
+import DetailMixins from 'mixins/Detail'
+import ImageMixins from 'mixins/Image'
 
-  export default {
-    mixins: [DetailMixins, ImageMixins],
-    components: {DateTimePicker: require('@/DateTimePicker').default},
-    methods: {
-      async doSubmit()
-      {
-        const data = Object.assign({episode_owner_id: this.$route.params.id}, this.data)
-        await this.$thisApi.doCreate(data)
-        this.createSuccess()
-      },
-    },
-    mounted()
-    {
-      this.$bus.on('create.show', () =>
-      {
-        this.data = {
-          status: 'Y',
-          sources_url: {},
-        }
-        this.show()
-      })
-    },
-    destroyed()
-    {
-      this.$bus.off('create.show')
-    },
+export default {
+  components: { DateTimePicker: require('@/DateTimePicker').default },
+  mixins: [DetailMixins, ImageMixins],
+  mounted () {
+    this.$bus.on('create.show', () => {
+      this.data = {
+        status: 'Y',
+        sources_url: {}
+      }
+      this.show()
+    })
+  },
+  destroyed () {
+    this.$bus.off('create.show')
+  },
+  methods: {
+    async doSubmit () {
+      const data = {
+        episode_owner_id: this.$route.params.id,
+        ...this.data,
+        sources_url: _.pickBy(this.data.sources_url, t => !!t)
+      }
+      await this.$thisApi.doCreate(data)
+      this.createSuccess()
+    }
   }
+}
 </script>
