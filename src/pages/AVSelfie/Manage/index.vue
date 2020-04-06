99083ffdb713adf5f358db99e8013aca21479fa6
Cindy <ziishe1995@gmail.com>
2020-04-06 15:13:11 +0800

    逗號區分女優

--- src/pages/AVSelfie/Manage/index.vue
+++ src/pages/AVSelfie/Manage/index.vue
@@ -3,12 +3,16 @@
     <!-- begin breadcrumb -->
     <ol class="breadcrumb pull-right m-b-20">
       <li class="breadcrumb-item">
-        <router-link :to="{ name: 'welcome' }">首页</router-link>
+        <router-link :to="{ name: 'welcome' }">
+          首页
+        </router-link>
       </li>
       <li class="breadcrumb-item">
         <a href="javascript:;">成人自拍</a>
       </li>
-      <li class="breadcrumb-item active">自拍管理</li>
+      <li class="breadcrumb-item active">
+        自拍管理
+      </li>
     </ol>
 
     <!-- begin row -->
@@ -16,7 +20,9 @@
       <div class="panel panel-inverse" style="clear:both;">
         <!-- begin panel-heading -->
         <div class="panel-heading p-t-10">
-          <h4 class="text-white m-b-0">自拍管理</h4>
+          <h4 class="text-white m-b-0">
+            自拍管理
+          </h4>
         </div>
         <!-- end panel-heading -->
         <!-- begin panel-body -->
@@ -24,59 +30,59 @@
           <alert />
           <div class="row m-b-20 justify-content-end panel-search-box">
             <div class="col-sm-2">
-              <j-button type="add" @click="$bus.emit('create.show')"></j-button>
+              <j-button type="add" @click="$bus.emit('create.show')" />
             </div>
             <div class="col-sm-10 form-inline justify-content-end panel-search">
               <div class="form-group width-100 m-r-10">
-                <j-select title="片种" :datas="options.episode" v-model="search.is_censored" />
+                <j-select v-model="search.is_censored" title="片种" :datas="options.episode" />
               </div>
               <div class="form-group width-100 m-r-10">
                 <j-select
+                  v-model="search.region_id"
                   title="地区"
                   :datas="options.area"
-                  valueKey="id"
-                  v-model="search.region_id"
+                  value-key="id"
                 />
               </div>
               <div class="form-group width-100 m-r-10">
                 <j-select
+                  v-model="av_actress_id"
                   title="女优"
                   :datas="options.actress"
-                  valueKey="id"
-                  displayKey="name"
-                  v-model="av_actress_id"
+                  value-key="id"
+                  display-key="name"
                 />
               </div>
               <div class="form-group width-100 m-r-10">
                 <j-select
+                  v-model="search.cup_id"
                   title="罩杯"
                   :datas="options.cup"
-                  valueKey="id"
-                  displayKey="size"
-                  v-model="search.cup_id"
+                  value-key="id"
+                  display-key="size"
                 />
               </div>
               <div class="form-group width-100 m-r-10">
                 <j-select
+                  v-model="search.years_id"
                   title="年份"
                   :datas="options.year"
-                  valueKey="id"
-                  displayKey="title"
-                  v-model="search.years_id"
+                  value-key="id"
+                  display-key="title"
                 />
               </div>
               <div class="form-group width-100 m-r-10">
-                <j-select title="状态" :datas="options.status" v-model="search.status" />
+                <j-select v-model="search.status" title="状态" :datas="options.status" />
               </div>
               <div class="form-group m-r-10">
                 <input
+                  v-model="search.keyword"
                   type="text"
                   class="form-control"
                   placeholder="请输入名称"
-                  v-model="search.keyword"
-                />
+                >
               </div>
-              <j-button type="search" @click="doSubmit"></j-button>
+              <j-button type="search" @click="doSubmit" />
             </div>
           </div>
           <!-- begin table-responsive -->
@@ -84,26 +90,46 @@
             <table class="table table-striped table-box text-center">
               <thead>
                 <tr>
-                  <th class="width-30">#</th>
-                  <th class="width-200">图片</th>
+                  <th class="width-30">
+                    #
+                  </th>
+                  <th class="width-200">
+                    图片
+                  </th>
                   <th>名称</th>
                   <th>片种</th>
-                  <th class="width-100">地区</th>
+                  <th class="width-100">
+                    地区
+                  </th>
                   <th>女优</th>
-                  <th class="width-100">罩杯</th>
-                  <th class="width-100">类型</th>
-                  <th class="width-100">年份</th>
-                  <th class="width-100">浏览次数</th>
-                  <th class="width-100">状态</th>
-                  <th class="width-150">建立时间</th>
-                  <th class="width-100">操作</th>
+                  <th class="width-100">
+                    罩杯
+                  </th>
+                  <th class="width-100">
+                    类型
+                  </th>
+                  <th class="width-100">
+                    年份
+                  </th>
+                  <th class="width-100">
+                    浏览次数
+                  </th>
+                  <th class="width-100">
+                    状态
+                  </th>
+                  <th class="width-150">
+                    建立时间
+                  </th>
+                  <th class="width-100">
+                    操作
+                  </th>
                 </tr>
               </thead>
               <tbody>
                 <tr v-for="(data, index) in datas" :key="index">
                   <td>{{ startIndex + index }}</td>
                   <td class="td-img slider-img-td">
-                    <img :src="data.cover_url" @click="$bus.emit('image.show', data.cover_url)" />
+                    <img :src="data.cover_url" @click="$bus.emit('image.show', data.cover_url)">
                   </td>
                   <td>{{ data.title }}</td>
                   <td>
@@ -115,21 +141,23 @@
                   <td>{{ data.region.name }}</td>
                   <td>
                     <span
-                      style="margin-right:5px"
                       v-for="(i, index) in data.actress"
                       :key="index"
-                    >{{ i.name }}</span>
+                      style="margin-right:5px"
+                    >{{ i.name }}
+                      <span v-if="index!==data.actress.length-1">,</span>
+                    </span>
                   </td>
                   <td>{{ data.cup.size }}</td>
                   <td>
                     <span
-                      class="label label-warning"
-                      style="margin-right:5px"
                       v-for="(area, index) in data.genres"
                       :key="index"
+                      class="label label-warning"
+                      style="margin-right:5px"
                     >
                       {{
-                      area.title
+                        area.title
                       }}
                     </span>
                   </td>
@@ -137,8 +165,8 @@
                   <td>{{ data.years.title }}</td>
                   <td>{{ data.views }}</td>
                   <td>
-                    <i class="fas fa-lg fa-check-circle text-green" v-if="data.status === 'Y'"></i>
-                    <i class="fas fa-lg fa-times-circle text-danger" v-else></i>
+                    <i v-if="data.status === 'Y'" class="fas fa-lg fa-check-circle text-green" />
+                    <i v-else class="fas fa-lg fa-times-circle text-danger" />
                   </td>
                   <td>{{ data.created_at }}</td>
                   <td class="text-left">
@@ -146,9 +174,9 @@
                       type="episode"
                       :action="true"
                       @click="$router.push({ name: 'adult-selfie-episode', params: { id: data.id }, query: { name: data.title } })"
-                    ></j-button>
-                    <j-button type="edit" :action="true" @click="$bus.emit('update.show', data)"></j-button>
-                    <j-button type="delete" :action="true" @click="doDelete(data.id)"></j-button>
+                    />
+                    <j-button type="edit" :action="true" @click="$bus.emit('update.show', data)" />
+                    <j-button type="delete" :action="true" @click="doDelete(data.id)" />
                   </td>
                 </tr>
               </tbody>
@@ -156,7 +184,7 @@
           </div>
           <!-- end table-responsive -->
           <!-- pagination -->
-          <paginate :page="paginate.page" :lastPage="lastPage" @pageChange="pageChange" />
+          <paginate :page="paginate.page" :last-page="lastPage" @pageChange="pageChange" />
           <!-- end pagination -->
         </div>
       </div>
@@ -168,27 +196,27 @@
 </template>
 
 <script>
-import ListMixins from "mixins/List";
-import Enable from "constants/Enable";
-import CensoredStatus from "constants/CensoredStatus";
+import Enable from 'constants/Enable'
+import CensoredStatus from 'constants/CensoredStatus'
+import ListMixins from 'mixins/List'
 
 export default {
   mixins: [ListMixins],
   components: {
-    ImageContainer: require("@/Container/Image").default,
-    Create: require("./modal/create").default,
-    Update: require("./modal/update").default
+    ImageContainer: require('@/Container/Image').default,
+    Create: require('./modal/create').default,
+    Update: require('./modal/update').default
   },
   data: () => ({
-    av_actress_id: "",
+    av_actress_id: '',
     search: {
-      is_censored: "",
-      region_id: "",
+      is_censored: '',
+      region_id: '',
       av_actress: [],
-      cup_id: "",
-      years_id: "",
-      status: "",
-      keyword: ""
+      cup_id: '',
+      years_id: '',
+      status: '',
+      keyword: ''
     },
     options: {
       episode: CensoredStatus,
@@ -200,18 +228,22 @@ export default {
       cup: []
     }
   }),
-  api: "av_selfie.manage",
+  api: 'av_selfie.manage',
+  created () {
+    this.getOptions()
+    this.doSearch()
+  },
   methods: {
-    async doSubmit() {
+    async doSubmit () {
       if (this.av_actress_id) {
-          this.search.av_actress = [];
-          this.search.av_actress.push(this.av_actress_id);
+        this.search.av_actress = []
+        this.search.av_actress.push(this.av_actress_id)
       } else {
-        this.search.av_actress = [];
+        this.search.av_actress = []
       }
-      this.doSearch();
+      this.doSearch()
     },
-    async getOptions() {
+    async getOptions () {
       const res = await axios.all([
         this.$thisApi.getAreas(),
         this.$thisApi.getYears(),
@@ -219,7 +251,7 @@ export default {
         this.$thisApi.getActress(),
         this.$thisApi.getCups()
         // this.$thisApi.getStatus(),
-      ]);
+      ])
       this.options = Object.assign({}, this.options, {
         area: res[0].data,
         year: res[1].data,
@@ -227,12 +259,8 @@ export default {
         actress: res[3].data,
         cup: res[4].data
         // status: res[5].data,
-      });
+      })
     }
-  },
-  created() {
-    this.getOptions();
-    this.doSearch();
   }
-};
+}
 </script>
