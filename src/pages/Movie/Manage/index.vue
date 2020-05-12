b1a654b673f81d98dcff6c83660f7c9b675eb41a
Summer <mp4bj6ruby@gmail.com>
2020-05-12 14:45:56 +0800

    電影管理: 列表人氣和評分欄位加上filter顯示格式

--- src/pages/Movie/Manage/index.vue
+++ src/pages/Movie/Manage/index.vue
@@ -97,8 +97,8 @@
                   <td>{{ data.name }}</td>
                   <td>{{ data.region.name }}</td>
                   <td>{{ data.years.title }}</td>
-                  <td>{{ data.views }}</td>
-                  <td>{{ data.score }}</td>
+                  <td>{{ data.views | views }}</td>
+                  <td>{{ data.score | score }}</td>
                   <td>
                     <i v-if="data.status === 'Y'" class="fas fa-lg fa-check-circle text-green" />
                     <i v-else class="fas fa-lg fa-times-circle text-danger" />
